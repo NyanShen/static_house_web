e917e14a330f351ed087ce488fdf70dc9826188b
NyanShen <3354509516@qq.com>
2020-06-19 00:08:04 +0800

    update login register submit validation

--- js/login.js
+++ js/login.js
@@ -1,4 +1,54 @@
 $(document).ready(function () {
+    let loginByPhone = true;
+    let nameMapper = {
+        phone: '手机号码',
+        phoneCode: '手机验证码',
+        account: "登录账号",
+        password: "密码"
+    }
+
+    function clearErrorText(element) {
+        element.text('');
+    }
+
+    function validateRequired(element, errorElement, name) {
+        let count = 0;
+        if (!element.val()) {
+            count++;
+            errorElement.text(`${name}不能为空`);
+        }
+        return count;
+    }
+
+    function validatePattern(pattern, element, errorElement, name) {
+        let count = 0;
+        let regExpInstance = new RegExp(pattern);
+        if (!regExpInstance.test(element.val())) {
+            count++;
+            errorElement.text(`请输入正确的${name}`);
+        }
+        return count;
+    }
+
+    function validateMapper(fieldId, validateType, pattern = '') {
+        let element = $(`#${fieldId}`);
+        let errorElement = $(`#${fieldId}Error`);
+        let validates = {
+            "required": validateRequired(element, errorElement, nameMapper[fieldId]),
+            "pattern": validatePattern(pattern, element, errorElement, nameMapper[fieldId])
+        }
+        return validates[validateType]
+    }
+
+    function validator(validateItems) {
+        let count = 0;
+        validateItems.forEach(item => {
+            count = count + validateMapper(item.fieldId, item.type, item.pattern);
+        });
+        return count;
+    }
+
+    //切换登录方式
     $('.login-form .login-tab span').each(function (index) {
         $(this).click(function () {
             $('.login-form>.hide').removeClass('hide');
@@ -7,17 +57,20 @@ $(document).ready(function () {
             $(this).addClass('actived');
 
             if ($('#findPassBtn').hasClass('hide')) {
+                loginByPhone = false;
                 $('#findPassBtn').removeClass('hide');
             } else {
+                loginByPhone = true;
                 $('#findPassBtn').addClass('hide');
             }
         })
     })
 
+    //显示隐藏密码
     $('.login-form .login-pass .eyes-icon').click(function () {
-        var _this = $(this)
+        let _this = $(this)
         //获取同级兄弟input当前的密码框
-        var _input = _this.siblings('#loginPass');
+        let _input = _this.siblings('#loginPass');
         if (_this.hasClass('close')) {
             _this.removeClass('close');
             _this.addClass('open');
@@ -35,9 +88,9 @@ $(document).ready(function () {
         $('#countdown').css({
             display: 'block'
         });
-        var second = 60;
+        let second = 60;
         $('#countdown').text((second) + "秒");
-        var interval = setInterval(function () {
+        let interval = setInterval(function () {
             second--;
             $('#countdown').text((second) + "秒");
             if (second < 0) {
@@ -53,76 +106,97 @@ $(document).ready(function () {
 
 
     let phone = $('#phone');
-    let account = $('#account');
-    let loginPass = $('#loginPass');
     let phoneCode = $('#phoneCode');
     let loginBtn = $('#loginBtn');
-    let loginForm = $('#loginForm');
-    let phoneError = $('#phoneError');
-    let accountError = $('#accountError');
-    let loginPassError = $('#loginPassError');
-    let phoneCodeError = $('#phoneCodeError');
+    let signForm = $('#signForm');
+    let password = $('#password');
 
-    loginForm.on('focusin', '#phone,#phoneCode,#account,#loginPass', function (event) {
+    signForm.on('focusin', '#phone,#phoneCode,#account,#password', function (event) {
         let targetId = event.currentTarget.id;
-        let errorElement = $('#' + targetId + 'Error');
+        let errorElement = $(`#${targetId}Error`);
         clearErrorText(errorElement);
     })
 
-    phone.on('focusout', function () {
-        phoneValidate(phone, phoneError);
-    })
-
-    phoneCode.on('focusout', function () {
-        phoneCodeValidate(phoneCode, phoneCodeError);
-    })
-
-    account.on('focusout', function () {
-        accountValidate(account, accountError);
-    })
-
-    loginPass.on('focusout', function () {
-        loginPassValidate(loginPass, loginPassError);
+    signForm.on('focusout', '#phone,#phoneCode,#account,#password', function (event) {
+        let targetId = event.currentTarget.id;
+        let element = $(`#${targetId}`);
+        let errorElement = $(`#${targetId}Error`);
+        validateRequired(element, errorElement, nameMapper[targetId]);
     })
 
     loginBtn.on('click', function () {
-        phoneValidate(phone, phoneError);
-        phoneCodeValidate(phoneCode, phoneCodeError);
-        accountValidate(account, accountError);
-        loginPassValidate(loginPass, loginPassError);
+        let count = 0;
+        if (loginByPhone) {
+            let errorCount = validator([
+                {
+                    fieldId: 'phone',
+                    type: 'required',
+                },
+                {
+                    fieldId: 'phone',
+                    type: 'pattern',
+                    pattern: '^1[34589]\\d{9}$'
+                },
+                {
+                    fieldId: 'phoneCode',
+                    type: 'required',
+                }
+            ])
+            count = count + errorCount;
+        } else {
+            let errorCount = validator([
+                {
+                    fieldId: 'account',
+                    type: 'required',
+                },
+                {
+                    fieldId: 'password',
+                    type: 'required',
+                }
+            ])
+            count = count + errorCount;
+        }
+
+    })
+
+    $('#registerBtn').on('click', function () {
+        let count = 0;
+        let errorCount = validator([
+            {
+                fieldId: 'phone',
+                type: 'required',
+            },
+            {
+                fieldId: 'phone',
+                type: 'pattern',
+                pattern: '^1[34589]\\d{9}$'
+            },
+            {
+                fieldId: 'phoneCode',
+                type: 'required',
+            },
+            {
+                fieldId: 'password',
+                type: 'required',
+            }
+        ])
+        count = count + errorCount;
+        if (count) {
+            return;
+        }
+        app.request({
+            url: app.apiUrl('/user/register'),
+            data: {
+                mobile: phone.val(),
+                source: '1',
+                password: password.val(),
+                randCode: phoneCode.val()
+            },
+            type: 'POST',
+            dataType: 'json',
+            headers: {},
+            done: function (res) {
+            }
+        });
     })
 })
-
-function clearErrorText(element) {
-    element.text('');
-}
-
-function phoneValidate(phone, phoneError) {
-    let phoneVal = phone.val();
-    let phoneReg = new RegExp("^1[3456789]\\d{9}$");
-    if (!phoneVal) {
-        phoneError.text('手机号码不能为空');
-        return;
-    }
-    if (!phoneReg.test(phoneVal)) {
-        phoneError.text('请输入正确的手机号码');
-    }
-}
-
-function phoneCodeValidate(phoneCode, phoneCodeError) {
-    if (!phoneCode.val()) {
-        phoneCodeError.text('手机验证码不能为空');
-    }
-}
-
-function accountValidate(account, accountError) {
-    if (!account.val()) {
-        accountError.text('账户名不能为空');
-    }
-}
-
-function loginPassValidate(loginPass, loginPassError) {
-    if (!loginPass.val()) {
-        loginPassError.text('密码不能为空');
-    }
-}
